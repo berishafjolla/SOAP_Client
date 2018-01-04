77482818ab831ba422964e24a8c330d379569c1d
Fjolla Berisha <berishafjolla.fb@gmail.com>
2018-01-04 21:55:06 +0100

    A client to consume SOAP web services
    Knowing the URL to the SOAP Web Service or the WSDL of these Web Services, this software consumes the SOAP Web Services. Developed with C#.

---
+++ SOAP_Client/SOAPClientFolder/NullableGenericProperty.cs
@@ -0,0 +1,56 @@
+﻿namespace WebServiceStudio
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing.Design;
+
+    internal class NullableGenericProperty : ClassProperty
+    {
+		public NullableGenericProperty(Type[] possibleTypes, string name, object val)	: base(possibleTypes, name, val)
+        {
+					
+					if (possibleTypes.Length == 2)
+					{				
+						if (possibleTypes[1].FullName == "System.Int32")
+						{
+						
+						}
+						possibleTypes[1] = null;
+					}
+        }
+
+        protected override void CreateChildren()
+        {
+        }
+
+        public override object ReadChildren()
+        {
+            return this.Value;
+        }
+
+        public override string ToString()
+        {
+            string str = base.ToString();
+            if (this.Value == null)
+            {
+                return str;
+            }
+            return (str + " = " + this.Value.ToString());
+        }
+
+        [RefreshProperties(RefreshProperties.All), Editor(typeof(DynamicEditor), typeof(UITypeEditor)), TypeConverter(typeof(DynamicConverter))]
+        public object Value
+        {
+            get
+            {
+                return base.InternalValue;
+            }
+            set
+            {
+                base.InternalValue = value;
+                base.TreeNode.Text = this.ToString();
+            }
+        }
+    }
+}
+
